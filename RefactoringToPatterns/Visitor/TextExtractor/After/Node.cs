ad57c3aeb69c026899f396dcf9bb9b76146ccdc8
guo <gjw123_2000@163.com>
2024-01-21 18:09:39 +0800

    visitor pattern: TextExtractor

---
+++ RefactoringToPatterns/Visitor/TextExtractor/After/Node.cs
@@ -0,0 +1,31 @@
+﻿namespace RefactoringToPatterns.Visitor.TextExtractor.After
+{
+    public interface Node
+    {
+        void acceptVisitor(NodeVisitor visitor);
+    }
+
+    public class Tag : Node
+    {
+        public void acceptVisitor(NodeVisitor visitor)
+        {
+            visitor.visitTag(this);
+        }
+    }
+
+    public class LinkTag : Node
+    {
+        public void acceptVisitor(NodeVisitor visitor)
+        {
+            visitor.visitLinkTag(this);
+        }
+    }
+
+    public class StringNode : Node
+    {
+        public void acceptVisitor(NodeVisitor visitor)
+        {
+            visitor.visitStringNode(this);
+        }
+    }
+}
---
+++ RefactoringToPatterns/Visitor/TextExtractor/After/NodeVisitor.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace RefactoringToPatterns.Visitor.TextExtractor.After
+{
+    public interface NodeVisitor
+    {
+        void visitTag(Tag tag);
+        void visitLinkTag(LinkTag linkTag);
+        void visitStringNode(StringNode stringNode);
+    }
+
+    public class TextExtractorVisitor : NodeVisitor
+    {
+        private StringBuilder results = new StringBuilder();
+
+        public string extracText()
+        {
+            var nodes = new Node[3] {new Tag(), new LinkTag(), new StringNode()};
+            foreach (var node in nodes)
+            {
+                node.acceptVisitor(this);
+            }
+
+            return this.results.ToString();
+        }
+
+        public void visitLinkTag(LinkTag linkTag)
+        {
+            this.results.AppendLine($"visiting LinkTag");
+        }
+
+        public void visitStringNode(StringNode stringNode)
+        {
+            this.results.AppendLine($"visiting StringNode");
+        }
+
+        public void visitTag(Tag tag)
+        {
+            this.results.AppendLine($"visiting Tag");
+        }
+    }
+}
